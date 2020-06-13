923232ed747a13019a60271944da177d95e1b130
Velupriya <velupriya@thinkbridge.in>
2020-06-13 10:35:38 +0530

    edit signup functionality

--- signup/signup.js
+++ signup/signup.js
@@ -1,14 +1,71 @@
-window.onload = function () {
-    document.getElementById("firstHeadId").style.backgroundColor = "grey";
- // document.getElementsByClassName("firstHeadClass").style.backgroundColor = "grey";
- let firstHead = document.getElementsByClassName("firstHeadClass");
- Array.from(firstHead).forEach(element => {
-    element.style.backgroundColor = 'grey';
-});
-};
+// window.onload = function () {
+//     document.getElementById("firstHeadId").style.backgroundColor = "grey";
+//  // document.getElementsByClassName("firstHeadClass").style.backgroundColor = "grey";
+//  let firstHead = document.getElementsByClassName("firstHeadClass");
+//  Array.from(firstHead).forEach(element => {
+//     element.style.backgroundColor = 'grey';
+// });
+// };
 
 
 function newBalajiFunction() {
     alert("new repo");
 }
     
+
+function signup() {
+    let userName = document.getElementById("userName").value;
+    let password = document.getElementById("password").value;
+    var finalResult = `
+{
+    userName: ${userName},
+    password: ${password}
+}
+`
+    if (userName == '') {
+        alert("please fill userName");
+    } else if (password == '') {
+        alert("please fill password");
+    } else {
+
+        alert(finalResult);
+    }
+}
+
+function userName() {
+    ///  console.log(document.getElementById("userName").removeEventListener());
+}
+
+function userNamedown() {
+    console.log(document.getElementById("userNamedown").value);
+}
+
+function userNamepress() {
+    console.log(document.getElementById("userNamepress").value);
+}
+
+// removeEventListener
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.onload = function () {
+//     document.getElementById("firstHeadId").style.backgroundColor = "grey";
+//  // document.getElementsByClassName("firstHeadClass").style.backgroundColor = "grey";
+//  let firstHead = document.getElementsByClassName("firstHeadClass");
+//  Array.from(firstHead).forEach(element => {
+//     element.style.backgroundColor = 'grey';
+// });
+// };
