71f42d2f247713a5a28a4a078a31c70d2704d909
saisushmanth <saisushmanth@hotmail.com>
2020-06-13 20:05:08 +0530

    script

---
+++ signup/sunny.js
@@ -0,0 +1,15 @@
+<script>
+function check(form)
+{
+
+if(form.userid.value == "saisushmanth" && form.pwd.value == "sunny")
+{
+	return true;
+}
+else
+{
+	alert("Error Password or Username")
+	return false;
+}
+}
+</script>
